9f0ee477573746f4b166faecfd0bd0d0d118ea58
KeLi <kelicto@protonmail.com>
2021-06-02 17:54:32 +0800

    Updates code.

--- KeLi.HelloEntityFramework.SQLite/Utils/DbUtil.cs
+++ KeLi.HelloEntityFramework.SQLite/Utils/DbUtil.cs
@@ -6,6 +6,35 @@ namespace KeLi.HelloEntityFramework.SQLite.Utils
 {
     internal class DbUtil
     {
+        public static int Write<T>(T entity, Action<T> updater, Func<T, bool> finder) where T : class
+        {
+            if (entity is null)
+                throw new ArgumentNullException(nameof(entity));
+
+            if (updater is null)
+                throw new ArgumentNullException(nameof(updater));
+
+            if (finder is null)
+                throw new ArgumentNullException(nameof(finder));
+
+            using (var context = new MyDbContext())
+            {
+                var data = context.Set<T>();
+                var target = data.FirstOrDefault(finder);
+
+                if (target is null)
+                {
+                    context.Set<T>().Add(entity);
+
+                    return context.SaveChanges();
+                }
+
+                updater.Invoke(target);
+
+                return context.SaveChanges();
+            }
+        }
+
         public static int Insert<T>(T entity, Func<T, bool> finder = null) where T : class
         {
             if (entity is null)
@@ -16,7 +45,7 @@ namespace KeLi.HelloEntityFramework.SQLite.Utils
                 var data = context.Set<T>();
                 var target = finder is null ? data.FirstOrDefault() : data.FirstOrDefault(finder);
 
-                if (target is null)
+                if (finder is null || target is null)
                 {
                     context.Set<T>().Add(entity);
 
@@ -27,23 +56,35 @@ namespace KeLi.HelloEntityFramework.SQLite.Utils
             }
         }
 
-        public static T Delete<T>(Func<T, bool> finder = null) where T : class
+        public static T Delete<T>(Func<T, bool> finder) where T : class
         {
+            if (finder is null)
+                throw new ArgumentNullException(nameof(finder));
+
             using (var context = new MyDbContext())
             {
                 var data = context.Set<T>();
-                var target = finder is null ? data.FirstOrDefault() : data.FirstOrDefault(finder);
+                var target = data.FirstOrDefault(finder);
 
                 return data.Remove(target);
             }
         }
 
-        public static int Update<T>(Action<T> updater, Func<T, bool> finder = null) where T : class
+        public static int Update<T>(Action<T> updater, Func<T, bool> finder) where T : class
         {
+            if (updater is null)
+                throw new ArgumentNullException(nameof(updater));
+
+            if (finder is null)
+                throw new ArgumentNullException(nameof(finder));
+
             using (var context = new MyDbContext())
             {
                 var data = context.Set<T>();
-                var target = finder is null ? data.FirstOrDefault() : data.FirstOrDefault(finder);
+                var target = data.FirstOrDefault(finder);
+
+                if (target is null)
+                    return 0;
 
                 updater.Invoke(target);
 
